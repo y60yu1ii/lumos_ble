a9a260a88c0af544470ea4942e225bab0013d0f1
yyl <y60yu1ii@gmail.com>
2019-01-21 10:26:28 +0800

    add uart ble

--- app/src/main/java/de/fishare/lumosbledemo/MainActivity.kt
+++ app/src/main/java/de/fishare/lumosbledemo/MainActivity.kt
@@ -65,9 +65,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val centralSetting = object :CentralManager.Setting{
-        override fun getNameRule(): String {
-            return "(AMB)-[a-zA-Z0-9]{4}"
-        }
+//        override fun getNameRule(): String {
+//            return "(AMB)-[a-zA-Z0-9]{4}"
+//        }
 
         override fun getCustomAvl(device: BluetoothDevice): AvailObj {
             return BcastAvl(device)
